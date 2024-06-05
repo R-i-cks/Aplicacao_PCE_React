0f6fdacd9ca19122388e52b075af928a0d0eba6a
AnaCosta956 <a95694@alunos.uminho.pt>
2024-06-05 16:13:54 +0100

    Crud registos
    Adição das funções delete, put e get por id

--- rotas/registos.js
+++ rotas/registos.js
@@ -27,4 +27,49 @@ router.post('/', async (req, res) => {
     }
 });
 
+router.get('/:id', async(req, res)=> {
+    try{
+        const {id} = req.params;
+        const registo = await Registo.findById(id);
+        if (!registo){
+            return res.status(404).send('Registo não encontrado')
+        }
+        res.status(200).send(registo);
+    } catch (err){
+        console.error(err.message);
+        res.status(500).send('Erro no Servidor');
+    }
+});
+
+router.put('/:id', async(req, res)=>{
+    try {
+        
+        const{id} = req.params;
+        const registo = await Registo.findByIdAndUpdate(id, req.body);
+        if (!registo){
+            return res.status(404).send('Registo não encontrado');
+        }
+        const updatedRegisto = await Registo.findById(id);
+        res.status(200).send(updatedRegisto)
+    } catch (error) {
+        console.error(err.message);
+        res.status(500).send('Erro no Servidor');
+    }
+});
+
+router.delete('/:id', async (req, res) => {
+    try {
+        const { id } = req.params;
+        const registo = await Registo.findByIdAndDelete(id);
+        if (!registo) {
+            return res.status(404).send('Registo não encontrado');
+        }
+        res.status(200).send(registo);
+    } catch (error) {
+        console.error(error.message);
+        res.status(500).send('Erro no Servidor');
+    }
+});
+
+
 module.exports = router;
